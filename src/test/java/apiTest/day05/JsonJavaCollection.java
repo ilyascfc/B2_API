7afa3ce96717b10e3ad016e684bb6ed138686760
USER <ilyascfc@gmail.com>
2023-01-25 18:42:09 +0300

    Add day 05 and update

---
+++ src/test/java/apiTest/day05/JsonJavaCollection.java
@@ -0,0 +1,37 @@
+package apiTest.day05;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+
+
+
+public class JsonJavaCollection {
+
+    @Test
+    public void UserToMap(){
+        Response response=given().accept(ContentType.JSON)
+                .when().get("https://demoqa.com/Account/v1/User/11");
+
+                        Assert.assertEquals(response.statusCode(),401);
+
+        Map<String,Object>jsonMap= response.body().as(Map.class);
+        System.out.println("jsonMap = " + jsonMap);
+
+        //verify the message
+        String message= (String) jsonMap.get("message");
+        System.out.println("message = " + message);
+        Assert.assertEquals(message,"User not authorized!");
+
+        //verify the code
+        String code= (String) jsonMap.get("code");
+        System.out.println("code = " + code);
+        Assert.assertEquals(code,"1200");
+    }
+}
